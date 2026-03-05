00dd31a040e5fea40d22709540fca2e0269c79e9
ejolles <jolleseric@gmail.com>
2026-03-05 12:18:01 +0100

    Updateing webite

--- js/script.js
+++ js/script.js
@@ -9,4 +9,24 @@ $(document).ready(function(){
             $(this).text('read more');
         }
     });
+
+    // Night mode toggle
+    $('#night-mode-toggle').click(function() {
+        $('body').toggleClass('night-mode');
+        
+        // Save preference to localStorage
+        if ($('body').hasClass('night-mode')) {
+            localStorage.setItem('nightMode', 'enabled');
+            $(this).html('<i class="fas fa-sun"></i>');
+        } else {
+            localStorage.setItem('nightMode', 'disabled');
+            $(this).html('<i class="fas fa-moon"></i>');
+        }
+    });
+
+    // Check for saved preference
+    if (localStorage.getItem('nightMode') === 'enabled') {
+        $('body').addClass('night-mode');
+        $('#night-mode-toggle').html('<i class="fas fa-sun"></i>');
+    }
 });
